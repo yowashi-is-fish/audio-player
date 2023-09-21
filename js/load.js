947d60d8604654efe41f3d65ba25c01b41210def
YOWASHI <129309849+yowashi-is-fish@users.noreply.github.com>
2023-09-21 19:54:28 +0900

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,159 +1,126 @@
 let successfully = 0;
 
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
+// DOM要素のキャッシュ
+const form = document.getElementById('uploadForm');
+const fileInput = document.getElementById('fileToUpload');
+const errorElement = document.getElementById('error');
+const resultElement = document.getElementById('result');
+const songsDiv = document.getElementById('Songs');
+const playlistElements = {
+  name: document.getElementById('Name'),
+  description: document.getElementById('Description'),
+  modified: document.getElementById('Modified'),
+  fileSize: document.getElementById('FileSize'),
+  songNum: document.getElementById('SongNum'),
+};
+
+// フォームのsubmitイベントリスナー
+form.onsubmit = function(event) {
   event.preventDefault(); // フォームのデフォルトの送信動作を無効化
 
-  var fileInput = document.getElementById('fileToUpload');
-  var playlist_error = document.getElementById('error');
-
-  var playlist_name = document.getElementById('Name');
-  var playlist_desc = document.getElementById('Description');
-  var playlist_mod  = document.getElementById('Modified');
-  var playlist_size = document.getElementById('FileSize');
-  var playlist_num  = document.getElementById('SongNum');
-
-  var songs_div = document.getElementById('Songs');
-
   // 初期化
-  playlist_name.textContent  = '';
-  playlist_desc.textContent  = '';
-  playlist_mod.textContent   = '';
-  playlist_size.textContent  = '';
-  playlist_num.textContent   = '';
+  for (const key in playlistElements) {
+    playlistElements[key].textContent = '';
+  }
 
-  if (successfully == 1) {
-    while(songs_div.lastChild){
-      songs_div.removeChild(songs_div.lastChild);
+  if (successfully === 1) {
+    while (songsDiv.lastChild) {
+      songsDiv.removeChild(songsDiv.lastChild);
     }
   }
 
-  playlist_error.innerHTML =  ''
+  errorElement.innerHTML = '';
 
   if (fileInput.files.length > 0) {
-    var file = fileInput.files[0];
-    var fileSize = file.size;
-    var reader = new FileReader();
+    const file = fileInput.files[0];
+    const fileSize = file.size;
+    const reader = new FileReader();
 
     reader.onload = function(e) {
       try {
-        // ファイルの内容をJSONとして解析
-        var playlistData = JSON.parse(e.target.result);
+        const playlistData = JSON.parse(e.target.result);
 
-        // 解析されたJSONデータをコンソールに表示
-        console.log(playlistData);
+        // データを表示
+        playlistElements.name.textContent = playlistData.Info.Name;
+        playlistElements.description.textContent = `説明 : ${playlistData.Info.Description}`;
+        playlistElements.modified.textContent = `変更日時 : ${playlistData.Info.Modified.Y}/${playlistData.Info.Modified.M}/${playlistData.Info.Modified.D} ${playlistData.Info.Modified.h}:${playlistData.Info.Modified.m}`;
+        playlistElements.fileSize.textContent = `ファイルサイズ : ${formatBytes(fileSize)} (${fileSize.toLocaleString()} Bytes)`;
+        playlistElements.songNum.textContent = `楽曲数 : ${playlistData.SongData.length} 曲`;
 
-        // データを表示するか、別の処理をここに追加
-        playlist_name.textContent = `${playlistData.Info.Name}`;
-        playlist_desc.textContent = `説明 : ${playlistData.Info.Description}`;
-        playlist_mod.textContent  = `変更日時 : ${playlistData.Info.Modified.Y}/${playlistData.Info.Modified.M}/${playlistData.Info.Modified.D} ${playlistData.Info.Modified.h}:${playlistData.Info.Modified.m}`;
-        playlist_size.textContent = `ファイルサイズ : ${formatBytes(fileSize)} (${fileSize.toLocaleString()} Bytes)`;
-        playlist_num.textContent  = `楽曲数 : ${playlistData.SongData.length} 曲`;
-
-        var song_count = 0;
+        // 曲の表示
+        let songCount = 0;
 
         for (const songData of playlistData.SongData) {
-          song_count ++;
+          songCount++;
 
-          var song_div   = document.createElement('div');
-          var song_title = document.createElement('h3');
-          var song_comp  = document.createElement('p');
-          var song_ext   = document.createElement('p');
-          var song_mime  = document.createElement('p');
-          var song_size  = document.createElement('p');
-          var song_file  = document.createElement('audio');
+          const songDiv = document.createElement('div');
+          songDiv.dataset.songId = songCount;
+          songsDiv.appendChild(songDiv);
 
-          song_div.dataset.songId = `${song_count}`;
-          songs_div.appendChild(song_div);
+          const songTitle = document.createElement('h3');
+          songTitle.textContent = songData.title; // titleをh3で表示
+          songDiv.appendChild(songTitle);
 
-          song_title.textContent = `${songData.title}`;
-          song_comp.textContent  = `作曲者 : ${songData.comp}`;
-          song_ext.textContent   = `拡張子 : ${songData.ext}`;
+          const composerP = document.createElement('p');
+          composerP.textContent = `作曲者 : ${songData.Comp}`; // Compを作曲者として表示
+          songDiv.appendChild(composerP);
 
-          let song_mimetype = null;
+          const elements = ['ext'];
+          elements.forEach((element) => {
+            const p = document.createElement('p');
+            p.textContent = element === 'ext' ? `拡張子 : ${songData[element]}` : `${element.charAt(0).toUpperCase() + element.slice(1)} : ${songData[element]}`;
+            songDiv.appendChild(p);
+          });
 
-          switch(songData.ext) {
-            case 'aac':
-              song_mimetype = 'audio/aac';
-              break;
+          const songMimeType = getSongMimeType(songData.ext);
+          const songSize = formatBytes(songData.song.length);
 
-            case 'amr':
-              song_mimetype = 'audio/amr';
-              break;
+          const songMimeP = document.createElement('p');
+          songMimeP.textContent = `MIMEタイプ : ${songMimeType}`;
+          songDiv.appendChild(songMimeP);
 
-            case 'awb':
-              song_mimetype = 'audio/amr-wb';
-              break;
+          const songSizeP = document.createElement('p');
+          songSizeP.textContent = `サイズ : ${songSize} (${songData.song.length.toLocaleString()} Bytes)`;
+          songDiv.appendChild(songSizeP);
 
-            case '3gb':
-            case '3gpp2':
-            case 'm4a':
-            case 'mp4':
-              song_mimetype = 'audio/mp4';
-              break;
+          const audio = document.createElement('audio');
+          audio.src = `data:${songMimeType};base64,${songData.song}`;
+          audio.setAttribute('controls', '');
+          audio.setAttribute('controlslist', 'nodownload');
+          audio.dataset.songTitle = songData.title;
+          songDiv.appendChild(audio);
 
-            case 'mp3':
-              song_mimetype = 'audio/mpeg';
-              break;
-
-            case 'mid':
-            case 'midi':
-            case 'xmf':
-            case 'rtttl':
-            case 'rtx':
-            case 'ota':
-              song_mimetype = 'audio/midi';
-              break;
-
-            case 'ogg':
-            case 'oga':
-              song_mimetype = 'application/ogg';
-              break;
-
-            case 'imy':
-              song_mimetype = 'audio/imelody';
-              break;
-
-            case 'wav':
-              song_mimetype = 'audio/x-wav';
-              break;
-
-            case 'smf':
-              song_mimetype = 'audio/sp-midi';
-              break;
-          }
-
-          song_mime.textContent = `MIMEタイプ : ${song_mimetype}`;
-          song_size.textContent = `サイズ : ${formatBytes(songData.song.length)} (${songData.song.length.toLocaleString()} Bytes)`;
-
-          song_file.src = `data:${song_mimetype};base64,${songData.song}`;
-          song_file.setAttribute('controls', '');
-          song_file.setAttribute('controlslist', 'nodownload')
-
-          var song_div = document.querySelector(`[data-song-id="${song_count}"]`);
-
-          song_div.appendChild(song_title);
-          song_div.appendChild(song_comp);
-          song_div.appendChild(song_ext);
-          song_div.appendChild(song_mime);
-          song_div.appendChild(song_size);
-          song_div.appendChild(song_file);
+          // ダウンロードボタンを追加
+          const downloadButton = document.createElement('button');
+          downloadButton.textContent = 'ダウンロード';
+          downloadButton.onclick = function () {
+            // ダウンロードリンクを生成してクリック
+            const blob = base64ToBlob(songData.song, songMimeType);
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${songData.title}.${songData.ext}`;
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            URL.revokeObjectURL(url);
+          };
+          songDiv.appendChild(downloadButton);
         }
 
         successfully = 1;
       } catch (error) {
-        // JSON解析エラーが発生した場合
         console.error(error.message);
-        playlist_error.innerHTML = `<p>ファイルが読み取れませんでした。<br><span style="display: inline-block;">ファイルが破損している可能性、</span><span style="display: inline-block;">または対応していないファイルの様です。</span></p><p>エラーメッセージ : ${error.message}</p>`;
-
+        errorElement.innerHTML = 'ファイルが読み取れませんでした。';
         successfully = 0;
       }
     };
 
-    reader.readAsText(file); // テキストファイルとして読み込む
+    reader.readAsText(file);
   } else {
-    result.innerHTML = 'ファイルが選択されていません。';
+    resultElement.innerHTML = 'ファイルが選択されていません。';
   }
-});
+};
 
 // バイトをキロバイト、メガバイトなどにフォーマットする関数
 function formatBytes(bytes, decimals = 2) {
@@ -166,4 +133,52 @@ function formatBytes(bytes, decimals = 2) {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+// 拡張子からMIMEタイプを取得する関数
+function getSongMimeType(ext) {
+  switch (ext) {
+    case 'aac':
+      return 'audio/aac';
+    case 'amr':
+      return 'audio/amr';
+    case 'awb':
+      return 'audio/amr-wb';
+    case '3gb':
+    case '3gpp2':
+    case 'm4a':
+    case 'mp4':
+      return 'audio/mp4';
+    case 'mp3':
+      return 'audio/mpeg';
+    case 'mid':
+    case 'midi':
+    case 'xmf':
+    case 'rtttl':
+    case 'rtx':
+    case 'ota':
+      return 'audio/midi';
+    case 'ogg':
+    case 'oga':
+      return 'application/ogg';
+    case 'imy':
+      return 'audio/imelody';
+    case 'wav':
+      return 'audio/x-wav';
+    case 'smf':
+      return 'audio/sp-midi';
+    default:
+      return '';
+  }
+}
+
+// Base64データをBlobに変換する関数
+function base64ToBlob(base64Data, mimeType) {
+  const byteCharacters = atob(base64Data);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: mimeType });
 }
